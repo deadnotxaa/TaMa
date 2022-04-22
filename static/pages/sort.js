2cac27fff02f192e1f065a84978b55a615bc822a
Terebellum <merlinusthewizard@gmail.com>
2022-04-22 14:41:31 +0300

    Index.html

---
+++ static/pages/sort.js
@@ -0,0 +1,11 @@
+console.log("Script Active")
+const params = {group: 'shared', draggable:'.list-group-item', ghostClass:"ghost", dragClass:"drag", chosenClass: "choose", forceFallback:true}
+var elements = document.getElementsByClassName('list-group')
+
+Array.from(elements).forEach(element => {
+    Sortable.create(element, params);
+});
+
+document.getElementById('hero-btn').addEventListener('click', function(){
+    document.getElementById('start-btn').scrollIntoView({block: 'end', behavior: 'smooth'})
+})
